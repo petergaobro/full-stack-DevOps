32db5bdd54b5d31ac3cab5a0d2bb28d83a10aa4b
PengGao1027 <37321872+petergaobro@users.noreply.github.com>
2024-08-29 12:14:19 +1000

    feat: ORM sequelize

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,35 +1,98 @@
-const { Client } = require("pg");
-require("dotenv").config();
-
-const connectPg = new Client({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
-connectPg
-  .connect()
-  .then(() => console.log(`Server is running on ${process.env.DB_PORT}`));
-
-connectPg.query("Select * from demotable", (err: any, res: any) => {
-  if (!err) {
-    console.log(res.rows);
-  } else {
-    console.log(err.message);
-  }
-  connectPg.end;
-});
-
-// const express = require("express");
+import express, { Application } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import Database from "./config/database";
 // const app = express();
-// require("dotenv").config();
+import * as dotenv from "dotenv";
 
-// app.get("/", (req: any, res: any) => {
-//   res.send("Hello, World!");
+dotenv.config();
+
+console.log("===> Hello, the backend server is starting ...");
+// @ts-ignore
+const port: number = parseInt(process.env.SERVER_PORT) || 8000;
+// const connectPg = new Client({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USERNAME,
+//   port: process.env.DB_PORT,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
 // });
 
-// app.listen(process.env.SERVER_PORT, () => {
-//   console.log(`Server is running on ${process.env.SERVER_PORT}`);
+// connectPg
+//   .connect()
+//   .then(() => console.log(`Database is running on ${process.env.DB_PORT}`));
+//
+// connectPg.query("Select * from demotable", (err: any, res: any) => {
+//   if (!err) {
+//     console.log(res.rows);
+//   } else {
+//     console.log(err.message);
+//   }
+//   connectPg.end;
 // });
+
+// app.get("/", async (req: any, res: any) => {
+//   try {
+//     const data = await myNewPool.query("SELECT * FROM schools");
+//     res.status(200).send(data.rows);
+//   } catch (err) {
+//     console.log(err);
+//     res.sendStatus(500);
+//   }
+// });
+//
+// app.post("/", async (req: any, res: any) => {
+//   const { name, location } = req.body;
+//   try {
+//     await myNewPool.query("INSERT INTO schools (name, address) VALUES ($1, $2)", [
+//       name,
+//       location,
+//     ]);
+//     res.status(200).send({ message: "Successfully added child" });
+//   } catch (err) {
+//     console.log(err);
+//     res.sendStatus(500);
+//   }
+// });
+//
+// app.get("/setup", async (req: any, res: any) => {
+//   try {
+//     await myNewPool.query(
+//       "CREATE TABLE schools( id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100))",
+//     );
+//     res.status(200).send({ message: "Successfully created table" });
+//   } catch (err) {
+//     console.log(err);
+//     res.sendStatus(500);
+//   }
+// });
+
+const corsOptions = {
+  origin: "http://localhost:4000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allow specific HTTP methods
+  maxAge: 86400, // Cache the preflight response for 24 hours
+  preflightContinue: true, // Pass the CORS preflight response to the next handler
+};
+
+class App {
+  public app: Application;
+
+  constructor() {
+    this.app = express();
+    this.app.use(cors(corsOptions));
+    this.databaseSync();
+    // this.plugins();
+    // this.routes();
+  }
+
+  protected databaseSync(): void {
+    Database.getInstance().sync().then();
+  }
+}
+
+const app = new App().app;
+
+app.listen(port, () => {
+  console.log("===> Application is listening at http://localhost:" + port);
+});
---
+++ api/src/config/database.ts
@@ -0,0 +1,68 @@
+import { Pool } from "pg";
+import { Sequelize } from "sequelize-typescript";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+// const myNewPool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USERNAME,
+//   port: process.env.DB_PORT,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+//
+// module.exports = myNewPool;
+
+class Database {
+  private static sequelize: Sequelize;
+
+  private static POSTGRES_DB = process.env.POSTGRES_DB as string;
+  private static POSTGRES_HOST = process.env.POSTGRES_HOST as string;
+  private static POSTGRES_PORT = process.env.POSTGRES_PORT as unknown as number;
+  private static POSTGRES_USER = process.env.POSTGRES_USER as unknown as string;
+  private static POSTGRES_PASSWORD = process.env
+    .POSTGRES_PASSWORD as unknown as string;
+
+  constructor() {
+    Database.connectToPostgreSQL().then();
+  }
+
+  private static async connectToPostgreSQL() {
+    this.sequelize = new Sequelize({
+      dialect: "postgres",
+      database: this.POSTGRES_DB,
+      username: this.POSTGRES_USER,
+      password: this.POSTGRES_PASSWORD,
+      host: this.POSTGRES_HOST,
+      port: this.POSTGRES_PORT,
+
+      define: {
+        schema: "e-commerce", // Set the schema name
+      },
+      models: [],
+      logging: (...msg) => console.log(msg),
+    });
+
+    // await this.sequelize.sync({alter: true});
+
+    await this.sequelize
+      .authenticate()
+      .then(() => {
+        console.log(
+          "✅ PostgreSQL Connection has been established successfully.",
+        );
+      })
+      .catch((err) => {
+        console.error("❌ Unable to connect to the PostgreSQL database:", err);
+      });
+  }
+
+  public static getInstance(): Sequelize {
+    if (!this.sequelize) {
+      Database.connectToPostgreSQL();
+    }
+    return this.sequelize;
+  }
+}
+
+export default Database;
