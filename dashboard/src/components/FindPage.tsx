2f69cff84598aaca82c96d59486fda634d8c9656
PengGao1027 <37321872+petergaobro@users.noreply.github.com>
2024-09-03 10:28:37 +1000

    add pagination for findings

--- dashboard/src/components/FindPage.tsx
+++ dashboard/src/components/FindPage.tsx
@@ -10,6 +10,7 @@ import {
   Typography,
   Box,
   Button,
+  TablePagination,
 } from "@mui/material";
 import { Finding } from "../types/scan-result.ts";
 
@@ -29,6 +30,8 @@ const mockFindings: Finding[] = [
 ];
 
 const FindingPage: React.FC = () => {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
   // handle route
   const navigate = useNavigate();
   // extract route parameters from the URL
@@ -40,6 +43,17 @@ const FindingPage: React.FC = () => {
   // back scan page button
   const handleScanPage = () => {
     navigate("/scan-list");
+  };
+
+  const handleChangePage = (_event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
   };
   return (
     // display a table of findings for a specific scan ID with MUI components
@@ -73,6 +87,15 @@ const FindingPage: React.FC = () => {
           </TableBody>
         </Table>
       </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 15]}
+        component="div"
+        count={mockFindings.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
       <Box sx={{ display: "flex", alignItems: "flex-start'" }}>
         <Button variant="outlined" onClick={handleBackHome}>
           Back home
